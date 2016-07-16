02d5a08d9eb33e33524959e1701fbb9faafdd8e1
DenisDubinin <dadubinin@gmail.com>
2016-07-16 12:48:05 +0200

    09: feature09

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(createServer);
 
 function createServer(req, res) {
-  res.end('08/feature-08');
+  res.end('09/feature-09');
 }
 
 server.listen(port, () => console.log(`server listening on port ${port}`));
