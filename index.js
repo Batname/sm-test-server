24580b3f1a93ae0d0ffa6deee1a5a6eaf487fbb6
DenisDubinin <dadubinin@gmail.com>
2016-07-16 09:23:09 +0200

    03: pipeline test
    03: add test error

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(createServer);
 
 function createServer(req, res) {
-  res.end('02/staging-test');
-  console.log('04/piplene-test-1, important change-2');
+  res.end('03/pipeline-test');
 }
 
 server.listen(port, () => console.log(`server listening on port ${port}`));
