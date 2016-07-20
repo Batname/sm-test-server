4937075e280e367aa943ec78f6b98a15f4ab1b19
DenisDubinin <dadubinin@gmail.com>
2016-07-20 17:33:42 +0200

    12: add bunyan

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 'use strict';
 
+const bunyan = require('bunyan');
 const http = require('http');
 const port = process.env.PORT || 3000;
+const log = bunyan.createLogger({name: "sm-test-server"});
 
 const server = http.createServer(createServer);
 
 function createServer(req, res) {
   res.end('08/feature-08');
+  log.info(res.statusCode);
 }
 
 server.listen(port, () => console.log(`server listening on port ${port}`));
